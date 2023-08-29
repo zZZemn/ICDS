f1c48eec5ab34f0fb644458ba84860fb1c656eb0
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-08-29 17:24:06 +0800

    walang kwenta

---
+++ admin/components/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ICDS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,900;1,200;1,500&family=Roboto+Condensed:wght@300;400&display=swap');
+    </style>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<nav class="navbar navbar-dark bg-dark text-light d-flex justify-content-beetween align-items-center">
+    <h1 class="ICDS">ICDS</h1>
+    <ul class="header-ul d-flex">
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+    </ul>
+</nav>
+
+<aside class="sidebar">
+    <ul class="list-group">
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-gauge"></i>
+                Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-gauge"></i>
+                Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-gauge"></i>
+                Dashboard
+            </a>
+        </li>
+    </ul>
+</aside>
+
+<script src="https://kit.fontawesome.com/c6c8edc460.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
---
+++ db/class.php
@@ -0,0 +1,7 @@
+<?php
+include('db.php');
+
+class admin_class
+{
+    
+}
