63d1b5c66c31b86a341d617845547dbac9f1f82f
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-09-22 23:07:06 +0800

    user nav

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$isLogin = false;
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $isLogin = true;
+}
+include('user-components/header.php');
+?>
+
+
+<?php
+include('user-components/footer.php');
---
+++ user-components/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ICDS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+    </style>
+    <link rel="stylesheet" href="user-css/styles.css">
+</head>
+
+<body>
+    <nav class="nav-container">
+        <img src="../../global-assets/store-logos/12719_store_75613.jpg">
+        <div class="nav-links">
+            <a href="#">Home</a>
+            <a href="#">Categories</a>
+            <a href="#">About us</a>
+            <a href="#">Blogs</a>
+            <a href="#">Contacts</a>
+            <a href="#">Meet our Team</a>
+        </div>
+        <a href="#">Login</a>
+    </nav>
