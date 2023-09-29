ff13ec3d303917aca2ee180d4fbae64b1d09a07b
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-09-29 21:42:07 +0800

    blogs

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $admin_db = new admin_class();
 </div>
 
 <div class="main-categories-container">
-    <h3>Our Categories</h3>
+    <h3 class="heading-h3">Our Categories</h3>
     <div class="container categories-container">
         <?php
         $getCategory = $admin_db->admin_Get_Categories();
@@ -45,6 +45,61 @@ $admin_db = new admin_class();
         <?php
         }
         ?>
+    </div>
+    <center><a href="#" class="read-more">Browse categories >></a></center>
+</div>
+
+<div class="about-us-container">
+    <center>
+        <h3 class="heading-h3">About us</h3>
+    </center>
+    <div class="container about-container">
+        <div class="map-container about-container-content-container">
+            <img src="global-assets/assets-used-in-web/ilo-ilo-city-map.png">
+        </div>
+        <div class="p-container about-container-content-container">
+            <p>The Directory was a fatal experiment in weak executive powers; it was created in reaction to the puritanical dictatorship that had existed under the Reign of Terror of 1793–94, and it would end up yielding to the more disciplined dictatorship of Napoleon Bonaparte. The Directory suffered from widespread corruption.
+                Directories are a place to provide quick snapshots of a business: what it does, how it does it, where it's located, possibly even with a map link and a link to its website. In this day and age of fast information, users appreciate quick and coherent information. In computing, a directory structure is the way an operating system arranges files that are accessible to the user. Files are typically displayed in a hierarchical tree structure.</p>
+            <a href="#" class="read-more">Read More</a>
+        </div>
+    </div>
+</div>
+
+<div class="blogs-container">
+    <center>
+        <h3 class="heading-h3">Blogs</h3>
+    </center>
+    <div class="container blogs-card-container">
+        <div class="blog-card">
+            <img src="global-assets/assets-used-in-web/blogs-img.png">
+            <div class="blog-contents-container">
+                <p class="jf2023">JOB FAIR 2023</p>
+                <h5>EASY PATH TO FIND JOBS</h5>
+                <article>August 17, 2023</article>
+                <p class="blog-content-para">A job fair is an event where multiple recruiters, hiring managers and employers can meet with potential employees in one convenient, neutral venue, such as a school or college. These fairs are usually managed by third-party recruitment consultants.</p>
+                <a href="#" class="read-more">READ MORE -></a>
+            </div>
+        </div>
+        <div class="blog-card">
+            <img src="global-assets/assets-used-in-web/blogs-img.png">
+            <div class="blog-contents-container">
+                <p class="jf2023">JOB FAIR 2023</p>
+                <h5>EASY PATH TO FIND JOBS</h5>
+                <article>August 17, 2023</article>
+                <p class="blog-content-para">A job fair is an event where multiple recruiters, hiring managers and employers can meet with potential employees in one convenient, neutral venue, such as a school or college. These fairs are usually managed by third-party recruitment consultants.</p>
+                <a href="#" class="read-more">READ MORE -></a>
+            </div>
+        </div>
+        <div class="blog-card">
+            <img src="global-assets/assets-used-in-web/blogs-img.png">
+            <div class="blog-contents-container">
+                <p class="jf2023">JOB FAIR 2023</p>
+                <h5>EASY PATH TO FIND JOBS</h5>
+                <article>August 17, 2023</article>
+                <p class="blog-content-para">A job fair is an event where multiple recruiters, hiring managers and employers can meet with potential employees in one convenient, neutral venue, such as a school or college. These fairs are usually managed by third-party recruitment consultants.</p>
+                <a href="#" class="read-more">READ MORE -></a>
+            </div>
+        </div>
     </div>
 </div>
 
